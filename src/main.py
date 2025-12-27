7b6858a6f07432fb6f51e52aedd986f9aa29d697
b9788 <B9788213@gmail.com>
2025-12-27 17:53:43 +0300

    test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from codegen import CodeGen
-from filemng import tokenfile, astfile, src
+from filemng import outfile,tokenfile, astfile, src
 from lexer import lex
 from parser import Parser
 from printast import get_ast
@@ -12,4 +12,4 @@ asm = CodeGen(ast).gen()
 
 with open(tokenfile, "w") as f: f.write("\n".join(str(t) for t in tokens))
 with open(astfile, "w", encoding="utf-8") as f: f.write(get_ast(ast))
-#with open(outfile, 'w') as f: f.write()
+with open(outfile, 'w') as f: f.write(asm)
